689907ae4247dae93b901d4b91990ce8188df482
Alexandro Oliveira <=>
2024-08-07 16:58:53 -0300

    Operadores em Javascript

---
+++ Modulo 01/M1S04/Operadores/js/script.js
@@ -0,0 +1,41 @@
+// 1 - Number 
+
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Operações aritmeticas
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+console.log(5 % 3);
+console.log(5+(4*2));
+
+//  Operações relacionais
+
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 >= 3);
+console.log(5 <= 3);
+console.log(5 == 3);
+
+// 3 - Especial number
+
+console.log(typeof Infinity); // Numero
+console.log(typeof NaN);//Numero
+console.log(Infinity + 1); //Infinity
+console.log(0 / 0); // NaN
+console.log(0 / "testo"); //NaN
+
+// 4 - String
+
+console.log("Hello");
+console.log('Hello');
+console.log(typeof "Hello World");
+console.log("Hello World".length);
+console.log("Hello World"[0]);
+console.log("Hello World".toUpperCase());
+console.log("Hello World".toLowerCase());
+
