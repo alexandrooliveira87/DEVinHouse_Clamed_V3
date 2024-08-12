7dfb638429745a1b9e0c3217a15098a132d061f1
Alexandro Oliveira <=>
2024-08-12 17:06:12 -0300

    Array e Objetos

---
+++ Modulo 01/M1S05/Array e Objetos/js/scripts.js
@@ -0,0 +1,44 @@
+/*
+// 1 - Arrays
+
+const lista = [1, 2, 3, 4, 5];
+
+console.log(lista[0]); // Imprime: 1
+console.log(lista); // Arrays
+console.log(typeof lista); //
+
+
+
+// 2 - Proproedades
+
+const numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros.length); // Imprime: 5
+
+const myName = 'Alex';
+
+console.log(myName.length); // Imprime: 4
+
+
+
+// 3 - metódos
+
+const numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros.push(6)); // Adiciona um elemento ao final do array e retorna o novo comprimento do array
+console.log(numeros); // [1, 2, 3, 4, 5, 6]
+
+*/
+
+// 5 - objetos
+
+const pessoa = {
+    nome: 'Alex',
+    idade: 25,
+    endereco: {
+        rua: 'Rua das Flores',
+        numero: 123
+    }
+};
+
+console.log(pessoa.nome, pessoa.idade,pessoa.endereco); // Imprime: Alex
