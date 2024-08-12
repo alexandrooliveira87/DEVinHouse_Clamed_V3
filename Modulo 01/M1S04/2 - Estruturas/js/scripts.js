3f983528b18b43a7e66bdba33724cd8a35eba740
Alexandro Oliveira <=>
2024-08-12 13:01:37 -0300

    Aula sobre estrururas

---
+++ Modulo 01/M1S04/2 - Estruturas/js/scripts.js
@@ -0,0 +1,103 @@
+/*
+// 1 - variaveis
+
+let nome = "John Doe";
+const idade = 30;
+let altura = 1.75;
+let peso = 80;
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+console.log(peso);
+
+
+
+ //  2 - promot
+
+ const age = prompt("Digite sua idade");
+
+ console.log(`Você tem ${age} anos`);
+
+
+
+ // 3 - alert
+
+alert("testando");
+
+
+// 4 - math
+
+console.log(Math.PI);
+console.log(Math.floor(Math.random() * 10));
+console.log(Math.max(5,2,1,10));
+console.log(Math.floor( 5.1585));
+
+
+// 5 - console
+
+console.log("Hello World");
+console.error("Erro ao realizar a operação");
+console.warn("Atenção, algo pode estar errado");
+console.time("Timer");
+console.timeEnd("Timer");
+
+
+// 6 - if / else
+
+    let numero = 5;
+
+    if (numero > 3) {
+        console.log("O número é maior que 3");
+    } else {
+        console.log("O número é menor ou igual a 3");
+    }
+
+    const m = 10;
+
+    if (m % 2 === 0) {
+        console.log("O número é par");
+    } else {
+        console.log("O número é ímpar");
+    }
+
+    const user = "Alexandro";
+
+    if (user === "Alexandro") {
+        console.log("Olá Alexandro!");
+    } else {
+        console.log("Olá visitante!");
+    }
+
+// 7 - if else if
+
+const num = 10;
+
+if (num > 10) {
+    console.log("O número é maior que 10");
+} else if (num > 5) {
+    console.log("O número é maior que 5 e menor ou igual a 10");
+} else {
+    console.log("O número é menor ou igual a 5");
+}
+
+const nome = "Alex";
+
+if (nome === "Alex") {
+    console.log("Olá Alex!");
+} else if (nome === "Bob") {
+    console.log("Olá Bob!");
+} else {
+    console.log("Olá visitante!");
+}
+
+*/ 
+
+// 8 - While
+
+let p = 0;
+
+while (p < 10) {
+    console.log(`repetindo ${p}`);
+    p++;
+} 
