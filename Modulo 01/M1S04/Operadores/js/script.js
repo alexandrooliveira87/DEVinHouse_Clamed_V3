72a28570015e7b3df7c81541748b9e31ec7c15bf
Alexandro Oliveira <=>
2024-08-12 11:58:07 -0300

    Aula dos tipos de operadores

--- Modulo 01/M1S04/Operadores/js/script.js
+++ Modulo 01/M1S04/Operadores/js/script.js
@@ -54,7 +54,56 @@ console.log("Hello\\World");
 // 6 - Concatenação
 
 console.log("Oi, " + " tudo" + " bem?");
-*/
+
 // 7 - template Strings
 
 console.log(`A soma de 2 + 2 é: ${2+2}` )
+
+// 8 - Bollean
+
+console.log(true);
+console.log(false);
+console.log(typeof true);
+console.log(typeof false);
+console.log(30 > 15);
+console.log(30 < 15);
+
+
+
+// 10 - Comparação
+
+console.log(5 == 5);
+console.log(5 === 5);
+console.log(5 != 5);
+console.log(5 !== 5);
+
+
+
+//11- Operadores lógicos
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+console.log(!true)
+console.log(!false)
+console.log(5>2 && 10 != 10)
+console.log(5>2 || 10 != 10)
+
+
+
+// 12 - empty values
+
+console.log(null);
+console.log(undefined);
+console.log(0);
+console.log("");
+console.log(false);
+*/ 
+
+// 13 - Mudanças de tipos
+
+console.log(typeof 5 + "3");
+console.log(5 + 3 + "3");
+console.log(5 + 3 + "3");
+console.log(String(5) + "3");
