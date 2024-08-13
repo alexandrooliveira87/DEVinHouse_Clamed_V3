763b8cdda255bdb55afd1beae6d0d94273160df2
Alexandro Oliveira <=>
2024-08-13 09:43:59 -0300

    Continuação sobre Objetos no JS

--- Modulo 01/M1S05/Array e Objetos/js/scripts.js
+++ Modulo 01/M1S05/Array e Objetos/js/scripts.js
@@ -28,7 +28,6 @@ const numeros = [1, 2, 3, 4, 5];
 console.log(numeros.push(6)); // Adiciona um elemento ao final do array e retorna o novo comprimento do array
 console.log(numeros); // [1, 2, 3, 4, 5, 6]
 
-*/
 
 // 5 - objetos
 
@@ -42,3 +41,64 @@ const pessoa = {
 };
 
 console.log(pessoa.nome, pessoa.idade,pessoa.endereco); // Imprime: Alex
+
+
+// 6 - criando e deletando propiedades
+
+const carro = {
+    motor: 2.0,
+    marca: "VW",
+    modelo: "Tiguan",
+    km: 20000,
+}
+
+console.log(carro); // { motor: 2.0, marca: 'VW', modelo: 'Tiguan', km: 20000 }
+
+carro.cor = "Azul";
+
+delete carro.motor;
+
+console.log(carro);
+
+
+// 7 - loop em arrays
+
+
+const users = ["Alexander", "João", "Manoel", "Maria"];
+
+for (let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuáiro ${users[i]}`);
+}
+ */
+
+// 8 - push e pop
+
+
+const array = ["A", "B", "C", "D", "E", "F"];
+
+array.push("G")
+
+console.log(array); // ["A", "B", "C", "D", "E", "F", "G"]
+
+console.log(array.length); 
+
+array.pop(array);
+
+console.log(array); 
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido); // G
+
+console.log(array); 
+
+
+
+
+
+
+
+
+
+
+
